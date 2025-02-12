07854a0d44dd6aabd0a09eea4212f6f4e281cdcc
GitHalex <calex1731@gmail.com>
2025-02-11 20:59:24 -0400

    Agregandu nuevos valores de typios de datos

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"strconv"
 )
 
+const PI float64 = 3.14 // global constant
+
 func main() {
 	fmt.Println("Hello World")
 	fmt.Println((4 <= 6))
@@ -21,5 +23,15 @@ func main() {
 	fmt.Println("valor float => " + strconv.FormatFloat(f, 'f', -1, 64))
 	var nombre string = "CloudLex"
 	fmt.Println("valor nombre => ", nombre, " ", "Otro")
+	fmt.Println("el valor %v", nombre)
+
+	fmt.Println("#############################")
+	var radius float64 = 5.154
+	var area float64
+
+	area = PI * radius * radius
+	fmt.Printf("Radius: %.2f \nPI:%.1f \n", radius, PI)
+	fmt.Printf("Area of Circle is : %f", area)
+	fmt.Println("Thank You")
 
 }
